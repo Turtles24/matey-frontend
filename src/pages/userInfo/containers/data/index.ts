a48a8ed22a49aa42ddd9ecef8abce358f49a14eb
Kangnets <kangnets88@gmail.com>
2024-09-07 19:06:39 +0900

    menuInput 컴포 추가

---
+++ src/pages/userInfo/containers/data/index.ts
@@ -0,0 +1,4 @@
+// index.ts
+export const years = Array.from({ length: 2025 - 1980 }, (_, i) => (1980 + i).toString());
+export const months = Array.from({ length: 12 - 1 }, (_, i) => (1 + i).toString());
+export const days = Array.from({ length: 30 - 1 }, (_, i) => (1 + i).toString());
